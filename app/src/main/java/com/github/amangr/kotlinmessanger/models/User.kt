02e0288628798996d44c7522556696d62881cbfa
AmanGr <alphagr110@gmail.com>
2021-07-30 23:56:12 +0300

    some chenges

--- app/src/main/java/com/github/amangr/kotlinmessanger/models/User.kt
+++ app/src/main/java/com/github/amangr/kotlinmessanger/models/User.kt
@@ -3,7 +3,7 @@ package com.github.amangr.kotlinmessanger.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-
+//user
 @Parcelize
 class User(val uid: String, val username: String, val profileImageUri: String): Parcelable {
     constructor() : this("","","")
